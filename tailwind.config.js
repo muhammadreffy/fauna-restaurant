6817ed00930ed7adde68c450febc8014a0f02c36
Muhammad Reffy <syahnataa@gmail.com>
2023-12-21 15:51:37 +0700

    web restaurant

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        nunito: ['Nunito']
+      },
+      colors: {
+        primary: '#06171C',
+        primer: '#16D9E3'
+      }
+    },
+  },
+  plugins: [],
+}
